40df7300fd789eb07ae7a1b1e80776331944fe15
eugene <lonforton@gmail.com>
2019-06-23 22:44:25 +0300

    Initial commit

---
+++ unit_tests.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE nickname_test_module
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(nickname_test_suite)
+
+BOOST_AUTO_TEST_CASE(nickname_input_test)
+{
+  BOOST_CHECK(1 == 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
