d5415f1bcc5e97936638144bb81d11e24c794018
eugene <lonforton@gmail.com>
2019-07-13 18:38:07 +0300

    input and output added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <memory>
 
-int main()
-{
-  std::cout << "Hello!" << std::endl;
+#include "radix_tree.h"
 
-  return 0;
-}
+int main() 
+{  
+  auto root = std::make_unique<TrieNode>();
+
+  std::string input_line;
+  while (std::getline(std::cin, input_line))
+  {
+    insert(root.get(), input_line);
+    print_trie(root.get());
+  }
+
+  print_trie(root.get());
+
+  print_minimum_prefixes(root.get());
+  
+  return 0; 
+} 
 
